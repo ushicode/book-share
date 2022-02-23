56e64c59b834337286cc5520aec0b73202573328
Collins Ushi <collinsushi@gmail.com>
2022-02-23 05:51:43 +0000

    Add screenshot and json path

---
+++ README.md
@@ -0,0 +1,9 @@
+# Book Share
+### Stack
+> Kotlin, MVVM 
+
+### The Application
+>   Book Share app 
+    Allows book lovers to share books
+
+> ![ScreenShot](/screenshots/1.png) 
--- app/src/main/java/com/collinsushi/bookshare/utility/Constant.kt
+++ app/src/main/java/com/collinsushi/bookshare/utility/Constant.kt
@@ -1,4 +1,4 @@
 package com.collinsushi.bookshare.utility
 
-const val WEB_SERVICE_URL = "https://nurhi.org/wp-content/uploads/2020/10/bookshare/"
+const val WEB_SERVICE_URL = "https://github.com/ushicode/book-share/tree/main/json/books.json"
 const val IMAGE_BASE_URL = "${WEB_SERVICE_URL}images/"
